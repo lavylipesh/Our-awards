8eaef1cb623203664cac6f16cee102b0bebb5f5d
lavylipesh <lilibethlangat@yahoo.com>
2019-10-18 18:12:09 +0300

    login_required decorator added to views

--- awards/views.py
+++ awards/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-
+@login_required(login_url='/accounts/register/')
 def index(request):
     return render(request,'index.html')
+
