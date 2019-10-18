78af0fe20c71e44948dfffcf14d608b575a3895b
lavylipesh <lilibethlangat@yahoo.com>
2019-10-18 17:22:03 +0300

    addig bootstrap

--- ourawards/settings.py
+++ ourawards/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'awards.apps.AwardsConfig',
+    'bootstrap4',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
