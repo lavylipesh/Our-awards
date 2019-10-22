5244ac585cbf65b7eb74b6afb98da828df24360f
lavylipesh <lilibethlangat@yahoo.com>
2019-10-22 07:59:57 +0300

    create register button in the login page

--- awards/views.py
+++ awards/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-@login_required(login_url='/accounts/register/')
+@login_required(login_url='/accounts/login/')
 def index(request):
     return render(request,'index.html')
    
